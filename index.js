d7de02aa93d80267103785f67425b1ecc278f4b1
Shreshth Agarwal <68282910+shreshthagarwal@users.noreply.github.com>
2025-08-24 13:54:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5000'],
+  origin: ['http://localhost:3000', 'http://localhost:5000', 'https://edu-leaderboard.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
